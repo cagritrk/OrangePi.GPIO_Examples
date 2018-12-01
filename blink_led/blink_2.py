7fa9effeae10b3be0fb34fbfa80ef667965da56f
cagritrk <cagri.turk@h2hsecure.com>
2018-12-01 23:16:18 +0300

    update blink_2

--- blink_led/blink_2.py
+++ blink_led/blink_2.py
@@ -5,6 +5,7 @@ from time import sleep          # this lets us have a time delay
 port = 7                        # set BCM7 (pin 26) as port (7)
 GPIO.setboard(GPIO.PCPCPLUS)    # Orange Pi PC board
 GPIO.setmode(GPIO.BOARD)        # set up BOARD BCM numbering
+GPIO.setwarnings(False)         # just for ingnore the bord already using warning
 
 GPIO.setup(port, GPIO.OUT)      # set BCM7 (pin 26) as an output (LED)
 
