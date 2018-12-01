dbec55af6ddea27cfb7a985ac3dfacb53987d08a
cagritrk <cagri.turk@h2hsecure.com>
2018-12-01 23:06:33 +0300

    blink example with datetime

---
+++ blink_led/blink_2.py
@@ -0,0 +1,30 @@
+import OPi.GPIO as GPIO
+import datetime
+from time import sleep          # this lets us have a time delay
+
+port = 7                        # set BCM7 (pin 26) as port (7)
+GPIO.setboard(GPIO.PCPCPLUS)    # Orange Pi PC board
+GPIO.setmode(GPIO.BOARD)        # set up BOARD BCM numbering
+
+GPIO.setup(port, GPIO.OUT)      # set BCM7 (pin 26) as an output (LED)
+
+print "Press CTRL+C to exit"
+
+date = datetime.datetime.now()
+
+print "Current time is ", date.hour , ":" , date.minute
+if int(date.minute) == 59:
+    minute = 00
+    hour = int(date.hour)+1
+else:
+    minute = int(date.minute)+1
+    hour = int(date.hour)
+print "It`ll be exit and set LED OFF automaticly on ", hour , ":" , minute
+
+while True:
+    date = datetime.datetime.now()
+    if int(date.minute) == minute:
+        GPIO.output(port, 0)       # set port/pin value to 1/HIGH/True
+        quit()
+    else:
+        GPIO.output(port, 1)       # set port/pin value to 0/LOW/False
